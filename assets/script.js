81176842a2e80bde3b969e67e3e67034c1ac8193
jacob zea <jacobzea96@gmail.com>
2023-01-10 19:33:20 -0700

    added test function, and prompt

--- assets/script.js
+++ assets/script.js
@@ -10,24 +10,39 @@ function writePassword() {
 
 }
 
+
+function makeid(length) {
+  var result = ``;
+  var characters = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()`;
+  var charactersLength = characters.length;
+  for (let index = 0; index < length; index++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength)); 
+  }
+  return result; 
+}
+
+console.log(makeid(13));
+
 // Pseudocode (SUPER IMPORTANT)
 // Array for each one 
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s""t","u","v","w","x","y","z"]
-var numbers = [0,1,2,3,4,5,6,7,8,9]
-var upperCase = ["A","B","C","D","E""F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var specialChar = ["?","!","#","$","%","&"]
+// var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+// var numbers = [0,1,2,3,4,5,6,7,8,9]
+// var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+// var specialChar = ["?","!","#","$","%","&"]
 
-var results = [lowerCase + numbers + upperCase + specialChar];
+// var results = [lowerCase + numbers + upperCase + specialChar];
 
-var lowerIndex = Math.floor(Math.random () * lowerCase.length);
-var numberIndex = Math.floor(Math.random () * numbers.length);
-var upperIndex = Math.floor(Math.random () * upperCase.length);
-var specialIndex = Math.floor(Math.random () * specialChar.length);
+// var lowerIndex = Math.floor(Math.random () * lowerCase.length);
+// var numberIndex = Math.floor(Math.random () * numbers.length);
+// var upperIndex = Math.floor(Math.random () * upperCase.length);
+// var specialIndex = Math.floor(Math.random () * specialChar.length);
+
+window.prompt("How long do you want your password?");
+
 
 
 // User input
   // how many characters 
-    // 8-128
   // lowercase confirm
   // uppercase confirm
   // number confirm
