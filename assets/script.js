388fe47eebb92580bf452f12100630a2bf4a647a
jacob zea <jacobzea96@gmail.com>
2023-01-10 18:24:49 -0700

    added var to js and content to readme

--- assets/script.js
+++ assets/script.js
@@ -12,16 +12,22 @@ function writePassword() {
 
 // Pseudocode (SUPER IMPORTANT)
 // Array for each one 
-  // var lowerCase -["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s""t","u","v","w","x","y","z"]
-  // var nums =[0,1,2,3,4,5,6,7,8,9]
-  // var upperCase =["A","B","C","D","E""F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  // var special =["?","!","#","$","%","&"]
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s""t","u","v","w","x","y","z"]
+var numbers = [0,1,2,3,4,5,6,7,8,9]
+var upperCase = ["A","B","C","D","E""F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var specialChar = ["?","!","#","$","%","&"]
 
-  // var results = [];
+var results = [lowerCase + numbers + upperCase + specialChar];
+
+var lowerIndex = Math.floor(Math.random () * lowerCase.length);
+var numberIndex = Math.floor(Math.random () * numbers.length);
+var upperIndex = Math.floor(Math.random () * upperCase.length);
+var specialIndex = Math.floor(Math.random () * specialChar.length);
 
 
 // User input
   // how many characters 
+    // 8-128
   // lowercase confirm
   // uppercase confirm
   // number confirm
